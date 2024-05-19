28ee678160efd9066ad5384b35898046de34bb96
AmaliaSwastika <swastika.amalia@gmail.com>
2024-05-20 01:23:45 +0700

    Add Splash Screen

---
+++ lib/screen/splash_screen/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:dameyu/screen/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToLogin();
+  }
+
+  _navigateToLogin() async {
+    await Future.delayed(const Duration(seconds: 3), () {});
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEB6383), 
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/logo.png",
+              width: 800,
+              height: 800,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
