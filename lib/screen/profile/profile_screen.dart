5bf522cf076a4ba20f19c7e2d164522a437f0c5e
AmaliaSwastika <swastika.amalia@gmail.com>
2024-06-17 00:25:48 +0700

    Update Profile

--- lib/screen/profile/profile_screen.dart
+++ lib/screen/profile/profile_screen.dart
@@ -1,14 +1,41 @@
+import 'package:dameyu/theme/theme_color.dart';
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("ProfileScreen")),
-      body: const Center(
-        child: Text("ProfileScreen Page Content"),
+      backgroundColor: ThemeColor().whiteColor,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70.0),
+        child: AppBar(
+          backgroundColor: ThemeColor().pinkColor,
+          centerTitle: true,
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Image.asset(
+              'assets/logo2.png',
+            ),
+          ),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(40),
+              bottomRight: Radius.circular(40),
+            ),
+          ),
+        ),
       ),
     );
   }
